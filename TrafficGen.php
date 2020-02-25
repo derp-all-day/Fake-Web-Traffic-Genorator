c5af2c0caaf3f4e18d9e3faaeb1a62cc972337a0
Andrew <andrew.blalock.ops@gmail.com>
2020-02-25 00:04:56 -0500

    Update TrafficGen.php

--- TrafficGen.php
+++ TrafficGen.php
@@ -81,32 +81,20 @@ function box_in($string) {
 }
 
 
-function fetch_proxies( $type ) {
-    $types = array(
-    	'usa'=>'http://www.us-proxy.org/',
-    	'ssl'=>'http://www.sslproxies.org/',
-    	'uk'=>'http://free-proxy-list.net/uk-proxy.html',
-    	'socks'=>'http://www.socks-proxy.net/',
-    	'anonymous'=>'http://free-proxy-list.net/anonymous-proxy.html'
-    );
-    if(!isset($types[$type])) {
-        return "Usage: ?type={Proxy type}\nTypes: usa, ssl, uk, socks, anonymous";
-    }
-    $source = file_get_contents($types[strtolower($_GET['type'])]);
+function fetch_proxies() {
+    $source = file_get_contents('http://www.sslproxies.org/');
     preg_match_all('/<tbody>(.*?)<\/tbody>/is', $source, $matches);
-    $array = explode("\n", $matches[1][0]);
+    preg_match_all('/<tr>(.*?)<\/tr>/is', $matches[1][0], $matches);
     $return = array();
-    foreach($array as $line) {
-    	$line = explode(str_replace('</tr></td>','',str_replace('<tr><td>','',$line)),$line);
-    	if(isset($line[0]) && isset($line[1])) {
-    		$return[] = "{$line[0]}:{$line[1]}";
-    	}
+    foreach($matches[1] as $key => $val) {
+        preg_match_all('/<td>(.*?)<\/td>/is', $val, $m);
+        $return[] = "{$m[1][0]}:{$m[1][1]}";
     }
     return $return;
 }
 
 function rand_proxy() {
-	$proxies        = fetch_proxies('ssl');
+	$proxies        = fetch_proxies();
 	$key         = rand(0,25);
 	$proxy       = $proxies[$key];
 	while(!check_proxy($proxy)) {
